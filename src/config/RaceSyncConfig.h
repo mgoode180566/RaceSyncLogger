8ac6c5815f93649c612bed2bbece2425afe3ead8
Michael Goode <michaelgoode66@googlemail.com>
2026-09-01 10:34:40 +0100

    Test SD SPI at 4 MHz

--- src/config/RaceSyncConfig.h
+++ src/config/RaceSyncConfig.h
@@ -23,11 +23,12 @@ namespace RaceSyncConfig
 
     // microSD card - SPI mode.
     // Wiring: CS=10, MOSI=11, SCK=12, MISO=13.
+    // Reduced to 4 MHz for SD write/delete reliability testing.
     constexpr int SD_CS_PIN = 10;
     constexpr int SD_MOSI_PIN = 11;
     constexpr int SD_SCK_PIN = 12;
     constexpr int SD_MISO_PIN = 13;
-    constexpr uint32_t SD_SPI_FREQUENCY = 20000000;
+    constexpr uint32_t SD_SPI_FREQUENCY = 4000000;
 
     constexpr double LOG_START_SPEED_KMH = 10.0;
     constexpr double LOG_STOP_SPEED_KMH = 3.0;
